1447576f41e0f1d1fc05a62dd51f835cf0b5c5b8
TheEdward20 <lalooeduar123@gmail.com>
2025-09-22 17:11:10 -0600

    Cambios y nuevos columnas

--- KatPC.Api/Model/KatPCDatos.cs
+++ KatPC.Api/Model/KatPCDatos.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
 using System.Text.Json.Serialization;
 using System.Text.Json.Serialization;
 
@@ -37,6 +38,66 @@ namespace KatPC.Api.Model
         public string Graficos { get; set; }
 
         public string Tipograficos { get; set; }
+
+        public string Usooficina   { get; set; }
+
+        public string Maximaexigencia { get; set; }
+
+        public string Vidautil { get; set; }
+
+        public string Arranque { get; set; }
+
+        public string Videoconfe {  get; set; }
+
+        public string Navegacion { get; set; }
+
+        public string Excelpprograma { get; set; }
+
+        public string Escrituradocum { get; set; }
+
+        public string Edicionfotoscad { get; set; }
+
+        public string Edicionvideo { get; set; }
+
+        public string videojuego { get; set; }
+
+        public string Tecladoilumi { get; set; }
+
+        public string Windowshello { get; set; }
+
+        public string Puertosusb { get; set; }
+
+        public string Salidavideo { get; set; }
+
+        public string Wifi { get; set; }
+
+        public string Touchpad { get; set; }
+
+        public string Bluetooth { get; set; }
+
+        public string Cd { get; set; }
+
+        public string Puertoauxiliar { get; set; }
+
+        public string Bocinas { get; set; }
+
+        public string Pantllatactil { get; set; }  
+
+        public string Webcam { get; set; }
+
+        public string Pantallad { get; set; }
+
+        public string Botones { get; set; }
+
+        public string Grado { get; set; }
+
+        public string Condicionfisica { get; set; }
+
+        public string Interpretacion { get; set; }
+
+        public string Encargado { get; set; }
+
+        public string Fechaprueba { get; set; }
 
     }
 }
